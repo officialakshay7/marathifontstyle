9db17cf605f882dd272c442e8b32d403957de80d
Akshay Kumar <officialakshay71@gmail.com>
2026-08-19 11:08:18 +0530

    Refactor ads.js to comment out script injection and update index.html for improved formatting and structure; remove unused service worker file.

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -1,6 +1,6 @@
-const s = document.createElement('script');
-s.src = 'https://quge5.com/88/tag.min.js';
-s.dataset.zone = '264694';
-s.async = true;
-s.setAttribute('data-cfasync', 'false');
-document.head.appendChild(s);
+// const s = document.createElement('script');
+// s.src = 'https://quge5.com/88/tag.min.js';
+// s.dataset.zone = '264694';
+// s.async = true;
+// s.setAttribute('data-cfasync', 'false');
+// document.head.appendChild(s);
